dce47291c8cd03c6a587e0f7f3b629a88d3b579b
Juan Lozada <juanlozadacochaches@gmail.com>
2024-07-14 19:41:59 -0500

    add dependency

---
+++ AppZoologico/src/main/java/Security/PasswordUtils.java
@@ -0,0 +1,16 @@
+
+package Security;
+import org.mindrot.jbcrypt.BCrypt;
+
+public class PasswordUtils {
+    
+    // Metodo para hashear un password
+    public static String hashPassword (String plainTextPassword) {
+        return BCrypt.hashpw(plainTextPassword, BCrypt.gensalt(12));
+    }
+    
+    // Metodo para verificar el password
+    public static boolean verifyPassword (String plainTextPassword, String hashedPassword) {
+        return BCrypt.checkpw(plainTextPassword, hashedPassword);
+    }
+}
